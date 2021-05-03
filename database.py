f669968d72fc449ad70c801ca1bddb9a497a9ec7
Bishnu <bishnu@inai.io>
2021-05-03 19:33:30 +0530

    db name change

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Author: bishnu
 # @Date:   2021-04-21 18:00:41
-# @Last Modified by:   bishnu
-# @Last Modified time: 2021-04-29 15:54:18
+# @Last Modified by:   Bishnu
+# @Last Modified time: 2021-05-03 19:31:12
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@192.168.1.8:3306/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@192.168.1.8:3306/fastapi_blog"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
